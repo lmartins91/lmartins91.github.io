562d0d0c596abb3286a0f83d860f72226920b8f8
Luis Martins <luis.m.martins91@gmail.com>
2016-04-26 13:00:53 -0400

    Humble beginnings

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var callback = function() {
+  $('.skill').each(function() {
+    newWidth = $(this).parent().width() * $(this).data('percent');
+    $(this).width(0);
+    $(this).animate({ width: newWidth }, 1000);
+  });
+}
+
+$(document).ready(callback);
+
+var resize;
+window.onresize = function() {
+  clearTimeout(resize);
+  resize = setTimeout(function() {
+    callback();
+  }, 100);
+}
